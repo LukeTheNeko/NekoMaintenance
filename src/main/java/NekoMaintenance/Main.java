cd9fd6544a31b410c4d397714af467c2699e4dd5
LukeTheNeko <lucaslkzin25@gmail.com>
2023-11-28 19:02:34 -0300

    v1.0.2

--- src/main/java/NekoMaintenance/Main.java
+++ src/main/java/NekoMaintenance/Main.java
@@ -14,19 +14,14 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         msgF = new ConfigFile(this, "messages");
-        Bukkit.getConsoleSender().sendMessage(c("\n&6 \n" +
-
-                "&2Plugin Working!\n"+
-                "&2v1.0.0 by LukeTheNeko\n"+
-                "&2https://github.com/LukeTheNeko/NekoMaintenance\n"+
-                "&5"+
+        Bukkit.getConsoleSender().sendMessage(c("&f[Neko&5Maintenance&f] &aInicializado com sucesso." +
                 "\n" +
+                "&f╔╗╔╔═╗╦╔═╔═╗&5╔╦╗╔═╗╦╔╗╔╔╦╗╔═╗╔╗╔╔═╗╔╗╔╔═╗╔═╗\n" +
+                "&f║║║║╣ ╠╩╗║ ║&5║║║╠═╣║║║║ ║ ║╣ ║║║╠═╣║║║║  ║╣ \n" +
+                "&f╝╚╝╚═╝╩ ╩╚═╝&5╩ ╩╩ ╩╩╝╚╝ ╩ ╚═╝╝╚╝╩ ╩╝╚╝╚═╝╚═╝\n" +
                 "\n" +
-                "  _  _ ___ _  _____    __  __   _   ___ _  _ _____ ___ _  _   _   _  _  ___ ___ \n" +
-                " | \\| | __| |/ / _ \\  |  \\/  | /_\\ |_ _| \\| |_   _| __| \\| | /_\\ | \\| |/ __| __|\n" +
-                " | .` | _|| ' < (_) | | |\\/| |/ _ \\ | || .` | | | | _|| .` |/ _ \\| .` | (__| _| \n" +
-                " |_|\\_|___|_|\\_\\___/  |_|  |_/_/ \\_\\___|_|\\_| |_| |___|_|\\_/_/ \\_\\_|\\_|\\___|___|\n" +
-                "                                                                                \n"));
+                "&2v1.0.1 by LukeTheNeko\n" +
+                "&2https://github.com/LukeTheNeko/NekoMaintenance\n\n"));
 
         plugin = this;
         getCommand("maintenance").setExecutor(new commands());
