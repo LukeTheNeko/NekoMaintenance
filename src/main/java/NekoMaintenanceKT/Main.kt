75fefc0051bb37dc52ce8c1afbaaf633a2f0dedf
LukeTheNeko <lucasneko25@gmail.com>
2024-02-25 23:18:21 -0300

    v1.3.3

--- src/main/java/NekoMaintenanceKT/Main.kt
+++ src/main/java/NekoMaintenanceKT/Main.kt
@@ -15,7 +15,7 @@ class Main : JavaPlugin() {
     &f║║║║╣ ╠╩╗║ ║&5║║║╠═╣║║║║ ║ ║╣ ║║║╠═╣║║║║  ║╣ 
     &f╝╚╝╚═╝╩ ╩╚═╝&5╩ ╩╩ ╩╩╝╚╝ ╩ ╚═╝╝╚╝╩ ╩╝╚╝╚═╝╚═╝
     
-    &2v1.3.2 by LukeTheNeko
+    &2v1.3.3 by LukeTheNeko
     &2https://github.com/LukeTheNeko/NekoMaintenance
     
     """.trimIndent()))
